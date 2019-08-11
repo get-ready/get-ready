45d6f16c2e9592175380ac083533aeae84539668
Grzegorz Ziemonski <grzegorz.ziemonski@gmail.com>
2019-08-11 20:05:58 +0200

    Initial commit

---
+++ get-ready.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+SLASHED_PREFIX="get-ready$(pwd)"
+UPPER_PREFIX=${SLASHED_PREFIX//\//__}
+PREFIX=$(echo $UPPER_PREFIX | tr '[:upper:]' '[:lower:]')
+
+IMAGE_NAME=${PREFIX}__image
+CONTAINER_NAME=${PREFIX}__container
+
+RUNNING_CONTAINERS=$(docker ps)
+
+if [ "$1" == "stop" ]; then
+    docker stop $CONTAINER_NAME
+    exit
+fi
+
+if [[ $RUNNING_CONTAINERS != *$CONTAINER_NAME* ]]; then
+    docker build -t project .
+    docker build -t $IMAGE_NAME -f ~/.config/get-ready/default/Dockerfile ~/.config/get-ready/default
+    docker run -d -it --rm -v $(pwd):/src --name $CONTAINER_NAME $(cat ~/.config/get-ready/default/dockeropts) $IMAGE_NAME 
+fi
+
+if [ ! -z "$1" ]; then
+    docker exec -it $CONTAINER_NAME $1
+fi
